34bad3bb51968726b1e06711194793248b0140a0
Dean <deaneffi1414@gmail.com>
2023-04-05 17:03:25 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import navLogo from "../assests/logoNav.png";
+
+function App() {
+  return (
+    <>
+      <div className="App bg-sky-600">
+        App <img src={navLogo} className="w-16" alt="" />
+      </div>
+      ;
+    </>
+  );
+}
+
+export default App;
